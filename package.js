e3b2707275eae743ef6851468655950113a69883
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:51:14 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
     name: 'xerdi:toast',
-    version: '0.0.3',
+    version: '0.0.4',
     summary: 'Admin-LTE Toasts for MeteorJS',
     git: '',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.8.0');
+    api.versionsFrom('2.11.0');
     api.use([
         'ecmascript',
         'universe:i18n@1.32.6',
-        'blaze-html-templates@1.2.1',
+        'blaze-html-templates@2.0.0',
         'ostrio:flow-router-extra@3.9.0',
-        'xerdi:admin-lte@0.0.5'
+        'xerdi:admin-lte@0.0.8'
     ]);
     api.mainModule('toasts.js', 'client');
 });
